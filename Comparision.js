26cf622f242c69a0ad25eaa30438163bf3d9e493
Chitransh Saxena <chitranshsaxena115@gmail.com>
2025-06-26 15:47:52 +0530

    Add files via upload

---
+++ Comparision.js
@@ -0,0 +1,6 @@
+console.log(2 > 1)
+console.log(2 != 1)
+
+
+console.log("2" > 1); // this concerts the string to number for comparision 
+console.log(2 > "1")  // this concerts the string to number for comparision 
